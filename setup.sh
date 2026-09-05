e6ee5a5e986f2cbdd2ac3675e628c30126b275cf
Alexander <42148372+Nautilus45433@users.noreply.github.com>
2026-09-05 16:50:59 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,16 @@
+pkg update && pkg upgrade -y
+pkg install git rust make clang binutils tmux -y
+
+git clone https://github.com/valnesfjord/tg-w-proxy-rs.git
+
+cd tg-ws-proxy-rs
+cargo build --release
+cd ..
+
+curl https://raw.githubusercontent.com/Nautilus45433/tg-ws-proxy-rs-autosetup/refs/heads/master/start.sh > tmux-tgp-start.sh
+curl https://raw.githubusercontent.com/Nautilus45433/tg-ws-proxy-rs-autosetup/refs/heads/master/stop.sh > tmux-tgp-stop.sh
+
+
+echo ". tmux-tgp-start.sh" >> .bashrc
+
+. tmux-tgp-start.sh
