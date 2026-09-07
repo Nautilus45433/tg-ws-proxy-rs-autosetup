8c9a63a303056a75ad72dba89533cc9eaee2cf69
Alexander <42148372+Nautilus45433@users.noreply.github.com>
2026-09-07 14:01:39 +0700

    Create clean.sh

---
+++ clean.sh
@@ -0,0 +1,7 @@
+find tg-ws-proxy-rs -mindepth 1 ! -wholename "tg-ws-proxy-rs/target/release/tg-ws-proxy" -delete
+
+pkg uninstall git krb5 libexpat resolv-conf ldns libresolv-wrapper termux-auth libdb openssh libedit openssh-sftp-server
+pkg uninstall rust clang libandroid-execinfo libcompiler-rt libffi libicu libllvm libxml2 lld llvm ndk-sysroot rust-std-aarch64-linux-android zstd
+pkg uninstall make
+
+pkg clear
